c91baef981317ba7f43a39324601370f757819a1
ninsyloo <evelinareal@gmail.com>
2021-06-27 02:24:17 -0300

    staging

--- weather-website/src/App.js
+++ weather-website/src/App.js
@@ -1,6 +1,25 @@
+import React, {useState} from 'react';
+import {Route} from 'react-router-dom';
+import About from './components/about'
+import Cards from './components/cards';
+import City from './components/city';
+import Nav from './components/nav';
 import './App.css';
 
+const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
+
+
 function App() {
+
+  const [cities, setCities] = useState([]);
+
+  function onClose(id){}
+  
+  function onSearch(city){}
+  
+  function onFilter(){}
+
+
   return (
     <div className="App">
    
---
+++ weather-website/src/components/nav/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchBar from '../searchbar'
+
+export default function Nav(){
+
+    return(
+        <div className='nav-container'>
+
+        </div>
+    )
+}
