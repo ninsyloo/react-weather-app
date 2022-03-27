8b8e1929831561bc6d9cf27d42c51566ea09b404
ninsyloo <evelinareal@gmail.com>
2022-03-27 02:20:43 -0300

    r

--- weather-website/src/components/about/index.jsx
+++ weather-website/src/components/about/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './about.css'
-import nin from '../../assets/nins.jpg'
-/* import rainbow from '../../assets/rainbow.png'*/
+/* import nin from '../../assets/nins.jpg' */
+import rainbow from '../../assets/rainbow.png'
 import {Link} from 'react-router-dom'
 
 export default function About(){
@@ -10,12 +10,12 @@ export default function About(){
         <div className='about-container'>
             <div className='left'>
                 <h1>My first React Project</h1>
-                <h3>Made with love for my Henry class so I hope you enjoy it!</h3>
+                <img src={rainbow} alt='NiN'/>
+                <h3>I hope you enjoy it!</h3>
                 <Link to='/'><span className='btn-return'>Return!</span></Link>
             </div>
-            <div className='right'>
-                <img src={nin} alt='NiN'/>
-            </div>
+{/*             <div className='right'>
+            </div> */}
         </div>
     );
 
